43e8f71b99b5c0d89b1c48bbd9dcdc00b5c33fb0
Sujan Mahmud <sujancseruet@gmail.com>
2019-06-26 10:36:26 +0600

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+use App\Events\MessageSent;
+use App\Message;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/getAll', function() {
+	$messages = Message::take(200)->pluck('content');
+
+	return $messages;
+});
+
+Route::post('/post', function() {
+	$message = new Message();
+	$content = request('message');
+	$message->content = $content;
+	$message->save();
+
+	event(new MessageSent($content));
+
+	return $content;
+});
